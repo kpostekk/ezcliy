1b2b906cff4b04cd96e5ddd35fc0b09642e7d2c5
Krystian Postek <krystian@postek.eu>
2021-01-16 13:22:51 +0100

    restrict_to_positionals_only
    yeah i know, wrong branch

--- ezcliy/commands.py
+++ ezcliy/commands.py
@@ -19,9 +19,9 @@ class Command:
     description: Optional[str]
     values: list[str] = []
     legacy: dict[str, Parameter] = {}
-    only_positionals = False
+    restrict_to_positionals_only = False
     help: Helpman = Helpman()
-    help.description = 'Prints this message'
+    help.description = 'prints this message'
 
     @property
     @cache
@@ -58,8 +58,12 @@ class Command:
         return [p for p in self.__class__.__dict__.values() if isinstance(p, Positional)]
 
     def __help_check(self):
-        if self.help:
+        if self.help or len(self.values) == 0:
             self.help.render_help(self)
+
+    def __restriction_check(self):
+        if len(self.values) != len(self.positionals) and self.restrict_to_positionals_only:
+            raise TooManyValues(self.values, len(self.positionals))
 
     def dispatch(self, args: list[str]):
         """
@@ -77,10 +81,7 @@ class Command:
             args = arguments.pass_args(args)
 
         # Save cleaned values
-        self.values = [arg for arg in args if not arg.startswith('-')]
-
-        if len(self.values) != len(self.positionals) and self.only_positionals:
-            raise TooManyValues(self.values, len(self.positionals))
+        self.values = [arg for arg in args if not arg.startswith('-') and arg != '']
 
         def pass_values_to_positionals():
             for i, p in enumerate(self.positionals):
@@ -91,6 +92,8 @@ class Command:
             if self.values[0] in self.commands.keys():
                 # Shrink values
                 cmd_name, self.values = self.values[0], self.values[1:]
+                # Checks
+                self.__restriction_check()
                 # Invoke, has args, first is subcommand
                 if not self.help:
                     self.invoke()
@@ -100,10 +103,12 @@ class Command:
                 cmd.dispatch(args[args.index(cmd_name) + 1:])  # Passing only args after command
             else:
                 self.__help_check()
+                self.__restriction_check()
                 pass_values_to_positionals()
                 self.invoke()  # Has args, first isn't subcommand
         else:
             self.__help_check()
+            self.__restriction_check()
             pass_values_to_positionals()  # This should raise an error
             self.invoke()  # Has no args
 

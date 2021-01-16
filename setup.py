9d6bddef9f8299dd194a5da14949b28edd2ff53b
Krystian Postek <krystian@postek.eu>
2021-01-16 14:49:01 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,29 @@
 from distutils.core import setup
 from ezcliy import __version__
 
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
 setup(
-    name='ezcliy',
+    install_requires=["colorama==0.4.4", "pyyaml==5.3.1"],
+    name="ezcliy",
     version=__version__,
-    packages=['ezcliy'],
-    url='https://github.com/kpostekk/ezcliy',
-    license='Apache License',
-    author='Krystian Postek',
-    author_email='krystian@postek.eu',
-    description='Framework for creating CLI tools'
+    packages=["ezcliy"],
+    url="https://github.com/kpostekk/ezcliy",
+    license="Apache",
+    author="Krystian Postek",
+    author_email="krystian@postek.eu",
+    description="Framework for creating CLI tools",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3'
+    ],
+    long_description=readme()
 )
